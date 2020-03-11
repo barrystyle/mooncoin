bb72d862433b2e399eec43c6afebcdca5f78af3b
barrystyle <barrystyle@westnet.com.au>
2020-03-12 07:42:01 +0800

    correct the tests so they compile correctly

--- src/test/scrypt_tests.cpp
+++ src/test/scrypt_tests.cpp
@@ -22,11 +22,11 @@ BOOST_AUTO_TEST_CASE(scrypt_hashtest)
         inputbytes = ParseHex(inputhex[i]);
 #if defined(USE_SSE2)
         // Test SSE2 scrypt
-        scrypt_1024_1_1_256_sp_sse2((const char*)&inputbytes[0], BEGIN(scrypthash), scratchpad);
+        scrypt_1024_1_1_256_sp_sse2((const char*)&inputbytes[0], (char*)&scrypthash, scratchpad);
         BOOST_CHECK_EQUAL(scrypthash.ToString().c_str(), expected[i]);
 #endif
         // Test generic scrypt
-        scrypt_1024_1_1_256_sp_generic((const char*)&inputbytes[0], BEGIN(scrypthash), scratchpad);
+        scrypt_1024_1_1_256_sp_generic((const char*)&inputbytes[0], (char*)&scrypthash, scratchpad);
         BOOST_CHECK_EQUAL(scrypthash.ToString().c_str(), expected[i]);
     }
 }
